120bf3972ea175d021e4e49095fcced4e6417858
Anshit Poddar <84501047+anshitpoddar@users.noreply.github.com>
2023-03-27 13:56:14 +0530

    Add files via upload

---
+++ simple_plot-XYZ 27-3-23---subplot.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5,6]
+y=[2,5,6,7,10,13]
+z=[7,11,3,13,5,9]
+mylabels=[6,5,4,3,2,1]
+plt.pie(z,labels=mylabels)
+plt.show()
