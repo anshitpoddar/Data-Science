5ff61692caabcfbf78486fca327cabc62ec3cb84
Anshit Poddar <84501047+anshitpoddar@users.noreply.github.com>
2023-03-27 13:08:40 +0530

    simple_basic_codes_in_ploting

---
+++ simple_plot-star27-3-23.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5,6]
+y=[2,5,6,7,10,13]
+#z=[7,11,3,13,5,9]
+plt.plot(x,y,marker="*")
+
